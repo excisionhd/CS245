e91a2ef68832a745411a4a8fa801aaf2fa9ba3b2
excisionhd <32623341+excisionhd@users.noreply.github.com>
2017-10-08 20:46:24 -0700

    Add files via upload

--- Hangman.java
+++ Hangman.java
@@ -2,52 +2,89 @@ import javax.swing.*;
 import java.awt.*;
 
 
-public class Hangman extends JFrame{
+public class Hangman extends JPanel{
 
     private static final long serialVersionUID = -3872551417773200878L;
 
-    public Hangman(String title){
-        Toolkit tk = Toolkit.getDefaultToolkit();
-        JFrame frame = new JFrame(title);
-        //JButton button = new JButton();
-        MyPanel panel = new MyPanel();
-        //JLabel header = new JLabel("CS245 Quarter Project");
-        //JLabel credit = new JLabel("By Team FTP");
+    private JButton[] buttons = new JButton[10];
+    private int mistakes =0;
+    private JFrame frame;
 
-        //frame.setPreferredSize(new Dimension(width, height));
-        //frame.setMaximumSize(new Dimension(width, height));
-        //frame.setMinimumSize(new Dimension(width, height));
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-        frame.add(panel);
-        frame.pack();
+    public Hangman(){
+        loadGUI();
 
 
-        /*header.setOpaque(true);
-        header.setVerticalAlignment(JLabel.TOP);
-        header.setHorizontalAlignment(JLabel.CENTER);
+    }
 
+    public void loadGUI(){
+/*
+        int ascii = 65;
+        Container c3 = new Container();
+        c3.setLayout(new BoxLayout(c3,BoxLayout.LINE_AXIS));
+        //JPanel buttonPanel = new JPanel(new GridLayout(2,13));
 
-        credit.setOpaque(true);
-        credit.setVerticalAlignment(JLabel.BOTTOM);
-        credit.setHorizontalAlignment(JLabel.CENTER);
-        credit.setFont(new Font("Arial", Font.BOLD, 12));*/
+        for(int i = 0;i<buttons.length;i++,ascii++) {
 
-        //panel.add(header);
-        //panel.add(credit);
+            buttons[i] = new JButton(Character.toString((char) ascii));
+             buttons[i].setPreferredSize(new Dimension(30,30));
+            c3.add(buttons[i]);
+
+        }
 
 
 
 
+        add(c3);
+
+*/
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Font f = new Font("Arial", Font.BOLD, 32);
+        Font f2 = new Font("Arial", Font.BOLD, 16);
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setFont(f);
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+
+        g2.drawString("Hangman",0,25);
+
+        //horizontal bar
+        g2.fillRect(240,70,100,5);
+
+        //vertical bar
+        g2.fillRect(240,70,5,160);
+
+        //rope
+        g2.fillRect(340,70,5,30);
+
+        //horizontal bar
+        g2.fillRect(120,230,320,5);
 
 
+        //head
+        g2.drawOval(332,100,20,20);
+
+        //body
+        g2.drawLine(342,120,342,175);
+
+        //left arm
+        g2.drawLine(342, 120, 322, 160);
+
+        //right arm
+        g2.drawLine(342, 120, 362, 160);
+
+        //left leg
+        g2.drawLine(342, 175, 330, 220);
+
+        //right leg
+        g2.drawLine(342, 175, 353, 220);
 
 
+    }
 
 
-
+    public void drawPart(Graphics g){
 
     }
 
