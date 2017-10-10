a13a8d3f3dca52f443236795915d6a8c644a8b3f
excisionhd <32623341+excisionhd@users.noreply.github.com>
2017-10-09 17:25:38 -0700

    Add files via upload

--- Hangman.java
+++ Hangman.java
@@ -1,45 +1,99 @@
+import com.sun.corba.se.impl.orbutil.graph.Graph;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
-public class Hangman extends JPanel{
+public class Hangman extends JPanel implements ActionListener{
 
     private static final long serialVersionUID = -3872551417773200878L;
 
-    private JButton[] buttons = new JButton[10];
+    private JButton[] buttons;
+    private JLabel scoreLabel;
+    private int score;
+
+    private JLabel clockLabel;
     private int mistakes =0;
     private JFrame frame;
+    private String word;
+    private String choice;
 
     public Hangman(){
+
+        buttons = new JButton[26];
+        clockLabel = new JLabel("");
+        scoreLabel = new JLabel("Score: ");
         loadGUI();
+        clock();
+        word = "hello";
 
 
     }
 
+    public void clock(){
+        Thread clock = new Thread(){
+            public void run(){
+                try{
+                    for(;;) {
+                        Calendar cal = new GregorianCalendar();
+                        int day = cal.get(Calendar.DAY_OF_MONTH);
+                        int month = cal.get(Calendar.MONTH)+1;
+                        int year = cal.get(Calendar.YEAR);
+
+                        int second = cal.get(Calendar.SECOND);
+                        int minute = cal.get(Calendar.MINUTE);
+                        int hour = cal.get(Calendar.HOUR);
+
+                        clockLabel.setText("Time " + hour + ":" + minute + ":" + second + "      Date " + month + "/" + day + "/" + year);
+                        sleep(1000);
+                    }
+                } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        };
+        clock.start();
+    }
+
     public void loadGUI(){
-/*
+
         int ascii = 65;
-        Container c3 = new Container();
-        c3.setLayout(new BoxLayout(c3,BoxLayout.LINE_AXIS));
-        //JPanel buttonPanel = new JPanel(new GridLayout(2,13));
+
+
+        Container hold = new Container();
+        hold.setLayout(new BoxLayout(hold,BoxLayout.PAGE_AXIS));
+        hold.add(clockLabel);
+        hold.add(scoreLabel);
+        hold.add(Box.createRigidArea(new Dimension(50,280)));
+        Container buttonContainer = new Container();
+        hold.add(buttonContainer);
+        buttonContainer.setLayout(new GridLayout(2,13,2,0));
+
 
         for(int i = 0;i<buttons.length;i++,ascii++) {
 
             buttons[i] = new JButton(Character.toString((char) ascii));
-             buttons[i].setPreferredSize(new Dimension(30,30));
-            c3.add(buttons[i]);
+            buttons[i].setPreferredSize(new Dimension(30,30));
+            buttons[i].setBorder(null);
+            buttons[i].setFont(new Font("Arial", Font.BOLD, 10));
+            buttons[i].addActionListener(this);
+            buttonContainer.add(buttons[i]);
 
         }
 
+        add(hold);
 
 
-
-        add(c3);
-
-*/
     }
 
     public void paintComponent(Graphics g) {
+
         super.paintComponent(g);
         Font f = new Font("Arial", Font.BOLD, 32);
         Font f2 = new Font("Arial", Font.BOLD, 16);
@@ -50,7 +104,8 @@ public class Hangman extends JPanel{
         g2.drawString("Hangman",0,25);
 
         //horizontal bar
-        g2.fillRect(240,70,100,5);
+        g2.fillRect(240, 70, 100, 5);
+
 
         //vertical bar
         g2.fillRect(240,70,5,160);
@@ -61,31 +116,77 @@ public class Hangman extends JPanel{
         //horizontal bar
         g2.fillRect(120,230,320,5);
 
-
-        //head
-        g2.drawOval(332,100,20,20);
-
-        //body
-        g2.drawLine(342,120,342,175);
-
-        //left arm
-        g2.drawLine(342, 120, 322, 160);
-
-        //right arm
-        g2.drawLine(342, 120, 362, 160);
-
-        //left leg
-        g2.drawLine(342, 175, 330, 220);
-
-        //right leg
-        g2.drawLine(342, 175, 353, 220);
-
-
     }
 
 
-    public void drawPart(Graphics g){
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object o = e.getSource();
+        JButton b = null;
+        String buttonText = "";
+        Graphics2D g2 = (Graphics2D)this.getGraphics();
 
+        if(o instanceof JButton) {
+            b = (JButton) o;
+        }
+
+        if( b!=null){
+            setChoice(b.getText().toLowerCase());
+
+            if(mistakes == 0 && !word.contains(getChoice()))
+            {
+                g2.drawOval(332,100,20,20);
+                mistakes++;
+                b.setVisible(false);
+            }
+            else if (mistakes == 1 && !word.contains(getChoice()))// draw body
+            {
+                g2.drawLine(342, 120, 342, 175);
+                mistakes++;
+                b.setVisible(false);
+            }
+            else if (mistakes == 2 && !word.contains(getChoice())) {
+                g2.drawLine(342, 120, 322, 160);
+                mistakes++;
+                b.setVisible(false);
+            }
+
+            else if (mistakes == 3 && !word.contains(getChoice())) //draw right arm
+            {
+                g2.drawLine(342, 120, 362, 160);
+                mistakes++;
+                b.setVisible(false);
+            }
+
+            else if (mistakes == 4 && !word.contains(getChoice())) //draw left leg
+            {
+                g2.drawLine(342, 175, 330, 220);
+                mistakes++;
+                b.setVisible(false);
+            }
+
+            else if (mistakes == 5 && !word.contains(getChoice())) //draw right leg
+            {
+                g2.drawLine(342, 175, 353, 220);
+                mistakes++;
+                b.setVisible(false);
+            }
+
+            else if (mistakes <6 && word.contains(getChoice())){
+                score+=100;
+                scoreLabel.setText("Score: "+Integer.toString(score));
+                b.setVisible(false);
+
+            }
+
+            }
     }
 
+    public void setChoice(String s){
+        choice = s;
+    }
+
+    public String getChoice(){
+        return choice;
+    }
 }
