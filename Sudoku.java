8647f5879cc989daa18ab98cf51bf5a602368c0a
Jason Kaufman <jkaufman217@yahoo.com>
2017-10-29 23:05:21 -0700

    Add files via upload

--- Sudoku.java
+++ Sudoku.java
@@ -1,114 +1,91 @@
-package swingv1.pkg0;
-
-/**
- * *************************************************************
- * file: Sudoku.java author: Team FTP class: CS 245 - Programming Graphical User
- * Interfaces
- *
- * assignment: Swing Project v1.2 date last modified: 10/19/17
- *
- * purpose: This class is the Sudoku game panel
- *
- ***************************************************************
- */
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.text.FieldPosition;
-import java.text.NumberFormat;
-import java.text.ParsePosition;
 import javax.swing.*;
-import javax.swing.text.NumberFormatter;
 
-public class Sudoku extends JPanel implements ActionListener
-{
-
+public class Sudoku extends JPanel {
     private Game game;
     private int score;
-    private final int SUDOKU_WIDTH = 9;
-    private final int SUDOKU_HEIGHT = 9;
-    private JTextField[][] numbers;
-    private JButton submit;
-    private JButton quit;
+    private static final int CLUSTER = 3;
+    private static final int MAX_ROWS = 9;
+    private static final float FIELD_PTS = 32f;
+    private static final int GAP = 3;
+    private static final Color BG = Color.BLACK;
+    private static final Color SOLVED_BG = Color.LIGHT_GRAY;
+    public static final int TIMER_DELAY = 2 * 1000;
+    private JTextField[][] fieldGrid = new JTextField[MAX_ROWS][MAX_ROWS];
 
-    private JTextField[][] grid = new JTextField[SUDOKU_WIDTH][SUDOKU_HEIGHT];
-
-    public Sudoku(Game game, int score)
-    {
+    public Sudoku(Game game, int score) {
         this.game = game;
         this.score = score;
-        numbers = new JTextField[SUDOKU_WIDTH][SUDOKU_HEIGHT];
-        JButton submit = new JButton("Submit");
-        JButton quit = new JButton("Quit");
 
-        JPanel headPanel = new JPanel(new GridLayout(3, 3));
-        headPanel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
-        headPanel.setBackground(Color.BLACK);
-
-        JPanel[][] panel = new JPanel[3][3];
-
-        for (int i = 0; i < panel.length; i++)
-        {
-            for (int j = 0; j < panel[i].length; j++)
-            {
-                panel[i][j] = new JPanel(new GridLayout(3, 3, 1, 1));
-                panel[i][j].setBackground(Color.BLACK);
-                panel[i][j].setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
-                headPanel.add(panel[i][j]);
+        
+        JPanel mainPanel = new JPanel(new GridLayout(CLUSTER, CLUSTER));
+        mainPanel.setBorder(BorderFactory.createEmptyBorder(GAP, GAP, GAP, GAP));
+        mainPanel.setBackground(BG);
+        JPanel[][] panels = new JPanel[CLUSTER][CLUSTER];
+        for (int i = 0; i < panels.length; i++) {
+            for (int j = 0; j < panels[i].length; j++) {
+                panels[i][j] = new JPanel(new GridLayout(CLUSTER, CLUSTER, 1, 1));
+                panels[i][j].setBackground(BG);
+                panels[i][j].setBorder(BorderFactory.createEmptyBorder(GAP, GAP, GAP, GAP));
+                mainPanel.add(panels[i][j]);
             }
         }
 
-        for (int i = 0; i < grid.length; i++)
-        {
-            for (int j = 0; j < grid[i].length; j++)
-            {
-                JTextField currentPanel = new JTextField(2);
-                currentPanel.setHorizontalAlignment(JTextField.CENTER);
-                currentPanel.setFont(currentPanel.getFont().deriveFont(Font.BOLD, 32.0f));
-
-                grid[i][j] = currentPanel;
-
-                int rowPos = i / 3;
-                int colPos = i / 3;
-
-                panel[rowPos][colPos].add(grid[i][j]);
+        for (int row = 0; row < fieldGrid.length; row++) {
+            for (int col = 0; col < fieldGrid[row].length; col++) {
+                fieldGrid[row][col] = createField(row, col);
+                int i = row / 3;
+                int j = col / 3;
+                panels[i][j].add(fieldGrid[row][col]);
             }
         }
 
         setLayout(new BorderLayout());
-        add(headPanel, BorderLayout.CENTER);
-
-        submit.addActionListener(this);
-        quit.addActionListener(this);
-        submit.setToolTipText("Check if answers are correct");
-        quit.setToolTipText("Exit to game over screen");
+        add(mainPanel, BorderLayout.CENTER);
+        add(new JButton(new SolveAction("Solve")), BorderLayout.PAGE_END);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e)
-    {
-        Object o = e.getSource();
-        JButton b = null;
-        JTextField t = null;
+    private JTextField createField(int row, int col) {
+        JTextField field = new JTextField(2);
+        field.setHorizontalAlignment(JTextField.CENTER);
+        field.setFont(field.getFont().deriveFont(Font.BOLD, FIELD_PTS));
 
-        if (o instanceof JButton)
-        {
-            b = (JButton) o;
-        }
-        else if (o instanceof JTextField)
-        {
-            t = (JTextField) o;
-        }
+        return field;
+    }
 
-        if (t != null)
-        {
+    private class SolveAction extends AbstractAction {
 
+        public SolveAction(String name) {
+            super(name);
+            int mnemonic = (int) name.charAt(0);
+            putValue(MNEMONIC_KEY, mnemonic);
         }
 
-        if (b != null)
-        {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new Timer(TIMER_DELAY, new ActionListener() {
+                private int i = 0;
+                private int j = 0;
 
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    // MAX_ROWS is 9
+                    if (i == MAX_ROWS) {
+                        ((Timer) e.getSource()).stop();
+                    }
+                    if (j == MAX_ROWS) {
+                        i++;
+                        j = 0;
+                    }
+                    int number = (int) (MAX_ROWS * Math.random()) + 1;
+                    fieldGrid[i][j].setBackground(SOLVED_BG);
+                    fieldGrid[i][j].setText(String.valueOf(number));
+
+                    j++;
+                }
+            }).start();
         }
     }
 }
