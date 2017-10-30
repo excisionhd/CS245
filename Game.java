8fe4b1bacfe027ce2c9365c978a41c6a334d6217
excisionhd <32623341+excisionhd@users.noreply.github.com>
2017-10-29 22:59:04 -0700

    Add files via upload

--- Game.java
+++ Game.java
@@ -3,8 +3,8 @@
  * author: Team FTP
  * class: CS 245
  *
- * assignment: Swing Project v1.0
- * date last modified: 10/9/2017
+ * assignment: Swing Project v1.1
+ * date last modified: 10/19/17
  *
  * purpose: This is the main method which ties all other panels into
  * one frame.
@@ -13,9 +13,13 @@
 
 import javax.swing.*;
 import java.awt.*;
+
+import java.awt.event.KeyEvent;
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-public class Game {
+
+public class Game{
 
     final int FINAL_WIDTH = 600; //frame dimensions
     final int FINAL_HEIGHT = 400;
@@ -29,6 +33,7 @@ public class Game {
     Hangman hg;
     ButtonGame bg;
 
+
     public enum STATE{ //allows access to each panel
         Intro,
     }
@@ -36,7 +41,7 @@ public class Game {
     public static STATE gameState = STATE.Intro; //starts game in intro
 
 
-    public Game(){ //initializes each panel upon creating the frame
+    public Game() throws IOException { //initializes each panel upon creating the frame
 
         frame = new JFrame("CS245 Project");
         intro = new MyPanel(this);
@@ -45,6 +50,8 @@ public class Game {
         menu = new Menu(this);
         credits = new Credits(this);
 
+
+
         frame.setPreferredSize(new Dimension(FINAL_WIDTH, FINAL_HEIGHT));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
@@ -52,6 +59,30 @@ public class Game {
         frame.pack();
         frame.add(intro);
         frame.setVisible(true);
+
+        KeyboardFocusManager.getCurrentKeyboardFocusManager()
+                .addKeyEventDispatcher(new KeyEventDispatcher() {
+                    @Override
+                    public boolean dispatchKeyEvent(KeyEvent e) {
+                        if(KeyEvent.KEY_PRESSED == e.getID()){
+                            if(e.getKeyCode()==KeyEvent.VK_F1)
+                            JOptionPane.showMessageDialog(frame,
+                                    "                 " +
+                                            "Created By:" + "\n" +
+                                            "Amir Sotoodeh(012165218)" + "\n" +
+                                            "Jason Kaufman(012134941)" + "\n" +
+                                            "Jacob Young(010224552)" + "\n" +
+                                            "    CS245 Quarter Project" + "\n" +
+                                            "               Fall 2017");
+
+                        }
+                        if(KeyEvent.KEY_PRESSED == e.getID()){
+                            if(e.getKeyCode()==KeyEvent.VK_ESCAPE)
+                                System.exit(0);
+                        }
+                        return false;
+                    }
+                });
 
         if(gameState == STATE.Intro) { //create game state with intro screen
 
@@ -74,7 +105,7 @@ public class Game {
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new Game();
     } //run game
 }
